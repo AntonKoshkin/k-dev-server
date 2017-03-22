5d763a7de4f83a683823262af6a7cf3723787cc3
anton koshkin <scink@scink-mac.local>
2017-03-22 18:49:40 +0300

    подготавливает к запуску на сервере

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const express		= require('express');
+const path			= require('path');
 const personal		= require('./personal.routes');
 const portfolio	= require('./portfolio.routes');
 // const assets		= require('./assets.routes');
@@ -8,17 +10,18 @@ module.exports = app => {
 			res.setHeader('Access-Control-Allow-Origin', '*');
 			next();
 		});
-		app.put('/*', function(req, res, next) {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			next();
-		});
-		app.post('/*', function(req, res, next) {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			next();
-		});
+		// app.put('/*', function(req, res, next) {
+		// 	res.setHeader('Access-Control-Allow-Origin', '*');
+		// 	next();
+		// });
+		// app.post('/*', function(req, res, next) {
+		// 	res.setHeader('Access-Control-Allow-Origin', '*');
+		// 	next();
+		// });
 	}
 
 	personal(app);
 	portfolio(app);
+	app.use('/', express.static(path.join(__dirname, 'public/dist')));
 	// assets(app);
 };
